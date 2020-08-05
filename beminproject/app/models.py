9a67dadb3c8b7d77d987c4f9e873864816659f90
알파카 <rhehd721@naver.com>
2020-08-05 20:03:56 +0900

    뷰 + 모델

--- beminproject/app/models.py
+++ beminproject/app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+# 주문 = 가게이름, 가게사진, 가게소개
+class order(models.Model):
+
+    image = models.ImageField()
+    store = models.CharField( max_length=50) 
+    intro = models.models.TextField()
+    
+
+    def __str__(self):
+        return self.name
+
+# 주문내역 = 메뉴사진, 메뉴이름, 메뉴가격
+class order_list(models.Model):
+
+    image = models.ImageField()
+    menu = models.CharField( max_length=50)
+    price = models.IntegerField()
+    
+
+    def __str__(self):
+        return self.name
--- beminproject/app/views.py
+++ beminproject/app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def order(request):
+    return render(request, 'order.html')
+
+def order_list(request):
+    return render(request, 'order_list.html')
+
+def order_update(request):
+    return render(request, 'order_update.html')
+
