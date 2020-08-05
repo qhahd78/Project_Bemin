c18371adfcbd3b93e422e5937eb3728934f813ea
YuMinHa <qhahd78>
2020-08-05 20:49:55 +0900

    url copy

--- beminproject/beminproject/urls.py
+++ beminproject/beminproject/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path,include
-import app.views
+from django.urls import path
+from . import views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path('', views.home, name='home'),
+    path('create/', views.create, name='create'),
+    path('update/<int:item_id>', views.update, name='update'),
+    path('delete/<int:item_id>', views.delete, name='delete'),
 ]
