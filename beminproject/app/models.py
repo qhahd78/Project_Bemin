5a2b13721b054071b9f888a57a0c38d006db28e7
YuMinHa <qhahd78>
2020-08-05 20:49:28 +0900

    model copy

--- beminproject/app/models.py
+++ beminproject/app/models.py
@@ -1,23 +1,13 @@
 from django.db import models
 
+# Create your models here.
 
-# 주문 = 가게이름, 가게사진, 가게소개
-class order(models.Model):
-
-    image = models.ImageField()
-    store = models.CharField( max_length=50) 
-    intro = models.models.TextField()
+class Item(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField(default=0)
     
-
     def __str__(self):
         return self.name
-
-# 주문내역 = 메뉴사진, 메뉴이름, 메뉴가격
-class order_list(models.Model):
-
-    image = models.ImageField()
-    menu = models.CharField( max_length=50)
-    price = models.IntegerField()
     
 
     def __str__(self):
