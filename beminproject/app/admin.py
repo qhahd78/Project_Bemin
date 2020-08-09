005a6d944dd5280464e8ca585882bb611d781938
알파카 <rhehd721@naver.com>
2020-08-09 19:32:11 +0900

    admin

--- beminproject/app/admin.py
+++ beminproject/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Order, Item, Store
 
 # Register your models here.
+
+admin.site.register(Order)
+admin.site.register(Item)
+admin.site.register(Store)
